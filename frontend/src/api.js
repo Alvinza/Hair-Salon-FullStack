0f87a3c804fc6f6eb3f082235201e38cb0175ed7
Alvin Zondi <167542762+Alvinza@users.noreply.github.com>
2025-08-24 17:13:11 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+// Axios instance for API calls
+// Base URL points to my backend hosted on Render
 const API = axios.create({
   baseURL: "https://salon-backend-autl.onrender.com/api",
 });
 
-// Add a request interceptor to add token
+// Attach token (if exists) to every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
